30bb8af6c8fd7753a05af31f08f1024bfe188a14
Comrade Cat <harripotter2606@gmail.com>
2023-05-22 20:04:18 +0300

    Modified the Big O test

--- big_O_test.py
+++ big_O_test.py
@@ -7,7 +7,7 @@ def samples(n):
     return ([[randint(5,10) for i in range(n)] for j in range(n)], [[randint(5,10) for i in range(n)] for j in range(n)])
 
 def my_function_list(n: tuple):
-    return matrix_multiply_positive_integer(n[0], n[1])
+    return my_matrix_multiply(n[0], n[1])
 
 best, others = big_o.big_o(my_function_list, samples, min_n=10, max_n=128, n_measures=20)
 print(best)
